dba2f827d047e78118cd6ce242c9a395a975e4a4
Nitu Balwani <nitu.balwani@avanade.com>
2023-08-29 07:48:58 +0800

    Added Distance between points and Price Quoter exercise

---
+++ DistanceBetweenPoints/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace DistanceBetweenPoints
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the value of x1: ");
+            double x1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the value of y1: ");
+            double y1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the value of x2: ");
+            double x2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the value of y2: ");
+            double y2 = double.Parse(Console.ReadLine());
+
+            //calculate
+            double distance = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2));
+
+            Console.WriteLine($"Distance betwwen point 1  and point 2  is {distance:F2}");
+
+
+            
+        }
+    }
+}
--- PriceQuoter/Program.cs
+++ PriceQuoter/Program.cs
@@ -4,7 +4,120 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter Product Code and Quantity");
+            string productCode = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            double price = 0.0;
+            double discount = 0.15;
+
+
+            switch (productCode)
+            {
+                case "BG-127":
+                    if (quantity < 25)
+                    {
+                        price = quantity * 18.99;
+                        Console.WriteLine($"The price is {price:C}");
+                    }
+                    else if (quantity >= 25 && quantity < 50)
+                    {
+                        price = quantity * 17.00;
+                        Console.WriteLine($"The price is {price:C}");
+
+                    }
+                    else if (quantity >= 50 && quantity < 250)
+                    {
+                        price = quantity * 14.49;
+                        Console.WriteLine($"The price is {price:C}");
+                    }
+                    else if (quantity >= 250)
+                    {
+                        price = (quantity * 14.49) * discount;
+                        price -= discount;
+                        Console.WriteLine("You got discount");
+                        Console.WriteLine($"The price is {price:C}");
+
+
+
+                    }
+                   
+                    break;
+                case "WRTR-28":
+                    if (quantity < 25)
+                    {
+                        price = quantity * 125.00;
+                        Console.WriteLine($"The price is {price:C}");
+                    }
+                    else if (quantity >= 25 && quantity < 50)
+                    {
+                        price = quantity * 113.75;
+                        Console.WriteLine($"The price is {price:C}");
+
+                    }
+                    else if (quantity >= 50 && quantity < 250)
+                    {
+                        price = quantity * 99.99;
+                        Console.WriteLine($"The price is {price:C}");
+                    }
+                    else if (quantity >= 250)
+                    {
+                        price = (quantity * 99.99) * discount;
+                        price -= discount;
+                        Console.WriteLine("You got discount");
+                        Console.WriteLine($"The price is {price:C}");
+
+
+
+                    }
+                    break;
+                case "GUAC-8":
+                    if (quantity < 25)
+                    {
+                        price = quantity * 8.99;
+                        Console.WriteLine($"The price is {price:C}");
+                    }
+                    else if (quantity >= 25 && quantity < 50)
+                    {
+                        price = quantity * 8.99;
+                        Console.WriteLine($"The price is {price:C}");
+
+                    }
+                    else if (quantity >= 50 && quantity < 250)
+                    {
+                        price = quantity * 7.49;
+                        Console.WriteLine($"The price is {price:C}");
+                    }
+                    else if (quantity >= 250)
+                    {
+                        price = (quantity * 7.49) * discount;
+                        price -= discount;
+                        Console.WriteLine("You got discount");
+                        Console.WriteLine($"The price is {price:C}");
+
+
+
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid request");
+                    break;
+                
+
+
+
+
+
+
+
+
+
+
+            }
+             
+
+
+
+
         }
     }
 }
